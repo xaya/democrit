1b80261fc500a47412c1d4073021f1f388e3530c
Daniel Kraft <d@domob.eu>
2020-03-25 15:22:35 +0100

    Test for the GSP.
    Add a basic test (using the Python integration testing framework) for
    the Democrit GSP.  Since it is so simple, that is enough and we won't
    need specific unit tests.

---
+++ gsp/test.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+#   Democrit - atomic trades for XAYA games
+#   Copyright (C) 2020  Autonomous Worlds Ltd
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Basic test for the Democrit GSP."""
+
+from xayagametest.testcase import XayaGameTest
+
+import os
+import os.path
+
+
+class DemGspTest (XayaGameTest):
+
+  def __init__ (self):
+    top_builddir = os.getenv ("top_builddir")
+    if top_builddir is None:
+      top_builddir = ".."
+    binary = os.path.join (top_builddir, "gsp", "democrit-gsp")
+    super (DemGspTest, self).__init__ ("dem", binary)
+
+  def run (self):
+    self.collectPremine ()
+    # For some reason we have to split the single premine coin into
+    # multiple ones, otherwise the wallet does not like the reorg test.
+    sendTo = {
+      self.rpc.xaya.getnewaddress (): 100
+      for _ in range (5)
+    }
+    self.rpc.xaya.sendmany ("", sendTo)
+    self.generate (1)
+    self.expectGameState ({})
+
+    self.mainLogger.info ("Sending some basic moves...")
+    self.sendMove ("foo", 42)
+    self.sendMove ("bar", {})
+    self.sendMove ("baz", {"t": 42})
+    self.sendMove ("foo", {"t": "c"})
+    self.sendMove ("baz", {"t": "b", "x": "ignored"})
+    self.generate (1)
+    self.sendMove ("foo", {"t": "a"})
+    self.generate (1)
+    self.expectGameState ({
+      "foo": ["a", "c"],
+      "baz": ["b"],
+    })
+    reorgBlk = self.rpc.xaya.getbestblockhash ()
+
+    self.mainLogger.info ("Duplicate IDs are just ignored...")
+    self.sendMove ("foo", {"t": "c"})
+    self.sendMove ("baz", {"t": "b"})
+    self.generate (1)
+    self.expectGameState ({
+      "foo": ["a", "c"],
+      "baz": ["b"],
+    })
+
+    self.mainLogger.info ("Same ID with another name...")
+    self.sendMove ("bar", {"t": "a"})
+    self.sendMove ("bar", {"t": "b"})
+    self.sendMove ("bar", {"t": "c"})
+    self.sendMove ("foo", {"t": "b"})
+    self.generate (1)
+    self.expectGameState ({
+      "foo": ["a", "b", "c"],
+      "bar": ["a", "b", "c"],
+      "baz": ["b"],
+    })
+
+    self.mainLogger.info ("Testing reorg...")
+    self.generate (20)
+    oldState = self.getGameState ()
+    self.rpc.xaya.invalidateblock (reorgBlk)
+
+    self.expectGameState ({
+      "foo": ["c"],
+      "baz": ["b"],
+    })
+    self.sendMove ("foo", {"t": "x"})
+    self.generate (1)
+    self.expectGameState ({
+      "foo": ["c", "x"],
+      "baz": ["b"],
+    })
+
+    self.rpc.xaya.reconsiderblock (reorgBlk)
+    self.expectGameState (oldState)
+
+
+if __name__ == "__main__":
+  DemGspTest ().main ()
